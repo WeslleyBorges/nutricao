21e8a97026459f00101f09efa7124ff4c623cacb
Weslley Borges <weslleynfs@gmail.com>
2021-08-25 20:33:04 -0300

    Correção de bug que fazia com que a divisão da quantidade da porção pela quantidade de alimento fosse inteira

--- alimentacao/managers.py
+++ alimentacao/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Sum, F
+from django.db.models import Sum, F, ExpressionWrapper, FloatField, Func
 from datetime import date
 from django.apps import apps
 
@@ -11,13 +11,19 @@ class RefeicaoManager(models.Manager):
 
         Porcao = apps.get_model('alimentacao', 'Porcao')
 
-        multiplication_factor_expression = F('quantidade') / F('alimento__quantidade')
+        template = '%(function)s(%(expressions)s AS FLOAT)'
+
+        quantidade_porcao = Func(F('quantidade'), function='CAST', template=template)
+        quantidade_alimento = Func(F('alimento__quantidade'), function='CAST', template=template)
+
+        multiplication_factor_expression = ExpressionWrapper(quantidade_porcao / quantidade_alimento,
+                                                             output_field=FloatField())
 
         whole_day_info = Porcao.objects.filter(refeicao_alimento__in=refeicoes).aggregate(
             kcal=Sum(F('alimento__kcal') * multiplication_factor_expression),
-            carboidratos=Sum(F('alimento__carboidratos') * multiplication_factor_expression),
-            proteinas=Sum(F('alimento__proteinas') * multiplication_factor_expression),
-            gorduras=Sum(F('alimento__gorduras') * multiplication_factor_expression)
+            carboidratos=Sum(F('alimento__carboidratos') * multiplication_factor_expression, output_field=FloatField()),
+            proteinas=Sum(F('alimento__proteinas') * multiplication_factor_expression, output_field=FloatField()),
+            gorduras=Sum(F('alimento__gorduras') * multiplication_factor_expression, output_field=FloatField())
         )
 
         return whole_day_info
